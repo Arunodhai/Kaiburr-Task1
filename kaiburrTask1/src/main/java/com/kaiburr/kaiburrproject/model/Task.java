2d5c8d9e17402d217589ef4cf199709d6d2c62bc
Arunodhai <arunodhai007@gmail.com>
2024-01-12 17:05:18 +0530

    Task 1 Updated.

--- kaiburrTask1/src/main/java/com/kaiburr/kaiburrproject/model/Task.java
+++ kaiburrTask1/src/main/java/com/kaiburr/kaiburrproject/model/Task.java
@@ -1,12 +1,10 @@
 package com.kaiburr.kaiburrproject.model;
 
-import com.mongodb.client.model.CollationStrength;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.query.Collation;
 
 import java.util.Date;
 
